836a4eee161e1c93e13f8be3c76357ffec59a07f
Achmad Rahman Mawardi <achmadrahmanm@gmail.com>
2025-10-08 17:30:03 +0700

    label

--- script.sh
+++ script.sh
@@ -1,6 +1,21 @@
+# from
 docker build -t achmadrahmanm/from from
 
+# run
 docker build -t achmadrahmanm/run run
 docker image rm achmadrahmanm/run
 
 docker build -t achmadrahmanm/run run --progress=plain --no-cache
+
+# command
+docker build -t achmadrahmanm/command command
+docker image inspect achmadrahmanm/command
+
+docker container create --name command achmadrahmanm/command
+docker container start command
+docker container logs command
+docker container stop command
+docker container rm command
+
+# label
+docker build -t achmadrahmanm/label label
